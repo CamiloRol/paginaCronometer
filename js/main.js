4c08484454b8bf3a41f2fa91855b57e842e43c18
CamiloRol <arodriguez@cesde.net>
2024-11-05 18:03:32 -0500

    ajuste al js para que no se recargue solo el inconveniente radica en importar la clase cronometro

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
-import Cronometro from "./Cronometro";
+/* import Cronometro from "./Cronometro"; */
 
 const setCronometers = document.getElementById('setCronometers')
 const procInit = document.getElementById('procInit')
 const settersTime = document.getElementById('settersTime')
 const procCreated = document.getElementById('procCreated')
+let setTime
 
 //Obtencion de valores
 const getCronoValue = document.getElementById('getCronoValue').value
-const setTime = document.getElementById('setTime').value
 
 window.addEventListener('load', iniciar)
 
-let crono1 = new Cronometro(getCronoValue, setTime)
+/* let crono1 = new Cronometro(getCronoValue, setTime) */
 
 function iniciar() {
-    procInit.addEventListener('click', function () {
-        setCronometers.style.display = "none"
+    settersTime.style.display = "none"    
+    procInit.addEventListener('click', function (event) {
+        event.preventDefault();
+        setCronometers.style.display = "none";
         settersTime.style.display = "flex"
+        createdInit()
     })
+/*     procCreated.addEventListener('click', function () {
+        
+    }) */
+}
+
+function createdInit() {
+    for (let i = 1; i < getCronoValue; i++) {
+        settersTime.innerHTML += `
+                <div>
+                    <label for="">Ingrese Tiempo en minutos: </label>
+                    <input type="number" id="setTime${i}">
+                </div>
+                <button id="procCreated" class="btnStyling">Crear</button>`
+        setTime = document.getElementById(`setTime${i}`).value
+    }
 }
 
