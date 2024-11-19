ce4ccf710acad87884d51727a8623da758779cc2
CamiloRol <arodriguez@cesde.net>
2024-11-18 22:17:19 -0500

    Changing commit languages finished createdInit function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* import Cronometro from "./Cronometro"; */
+import Cronometro from "./Cronometro.js";
 
 const setCronometers = document.getElementById('setCronometers')
 const procInit = document.getElementById('procInit')
@@ -7,12 +7,10 @@ const procCreated = document.getElementById('procCreated')
 let setTime
 const reloadPage = document.getElementById('reloadPage')
 
-//Obtencion de valores
-const getCronoValue = document.getElementById('getCronoValue').value
-
 window.addEventListener('load', iniciar)
 
-/* let crono1 = new Cronometro(getCronoValue, setTime) */
+/* Aun no tengo certeza donde voy a ubicar el objeto cronometro como tal */
+let crono1 = new Cronometro(getCronoValue, setTime)
 
 /* lo que hacemos con window load es tener una funcion que nos precargue botones o funcionalidades iniciales para la pagina */
 function iniciar() {
@@ -20,27 +18,41 @@ function iniciar() {
     procInit.addEventListener('click', function (event) {
         event.preventDefault();
         setCronometers.style.display = "none";
-        createdInit()
+        createdInit();
     })
-/*  procCreated.addEventListener('click', function () {
-
-    }) */
 
     reloadPage.addEventListener('click', function () {
     location.reload()
     })
 }
 
-/* Esta es la funcion que solicita los tiempos que va a tener los cronos */
+/*  Esta es la funcion que solicita los tiempos que va a tener los cronos */
 function createdInit() {
     settersTime.style.display = "flex"
-    for (let i = 0; i < getCronoValue; i++) {
-        settersTime.insertAdjacentHTML("afterbegin",`
-                <div>
-                    <label for="">Ingrese Tiempo en minutos: </label>
-                    <input type="number" id="setTime${i}">
-                </div>
-                <button id="procCreated" class="btnStyling">Crear</button>`)
+    let content = '';
+
+    const getCronoValue = document.getElementById('getCronoValue').value
+
+    if (getCronoValue < 6) {
+        for (let i = 0; i < getCronoValue; i++) {
+            content += ` <div class="divTime"> 
+            <label for="setTime${i}">Ingrese Tiempo en minutos: </label> 
+            <input type="number" id="setTime${i}"> </div> `
+        }        
+    }else {
+        content += `<p>Estamos programados para brindarte la mejor experiencia por eso lo maximo que puedes insertar son 5 cronometros</p>`
     }
-    settersTime.insertAdjacentHTML("beforeend", ` <button id="procCreated" class="btnStyling">Crear</button> `)
+
+    content += ` <button id="procCreated" class="btnStyling">Crear</button> `
+
+    settersTime.innerHTML += content
+
+    document.addEventListener('click', function(event) { if (event.target && event.target.id === 'procCreated')
+        
+        {console.log('Botón Crear presionado');}
+    })
+}
+
+function createdCrono() {
+    let crono1 = new Cronometro(getCronoValue, setTime)
 }
